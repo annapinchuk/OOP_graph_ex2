b383e00d4313b3525e8c019b600b6086d8e3154d
annapinchuk <76442355+annapinchuk@users.noreply.github.com>
2021-12-14 00:08:39 +0200

    final

--- oop_ex2/src/clases/Ex2.java
+++ oop_ex2/src/clases/Ex2.java
@@ -43,7 +43,7 @@ public class Ex2 {
     public static void runGUI(String json_file) {
         DirectedWeightedGraphAlgorithms alg = getGrapgAlgo(json_file);
         // ****** Add your code here ******
-        //
+        //A
         // ********************************
         new My_GUI((Graph_Algo) alg);
     }
